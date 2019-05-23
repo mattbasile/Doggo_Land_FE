b9d081b3b6371dd203cae9a4c07087ad3ac7ba02
Matt Basile <matthewbasile2@gmail.com>
2019-05-23 08:42:05 -0400

    dog card completed

--- client/src/components/Visitor/HomePage/CardDisplay.js
+++ client/src/components/Visitor/HomePage/CardDisplay.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import DogCard from '../DogCard'
 
 export default function CardDisplay() {
     return (
-        <div>
-            
+        <>
+        <div className="blue-background w-full">
+            <div className="flex justify-between w-1/3 mx-auto h-24 items-center body-font  text-white">
+                <h3 className="text-5xl font-normal title-font tracking-wide">Dogs</h3>
+                <h3 className="text-5xl font-normal title-font tracking-wide">Kennels</h3>
+            </div>
         </div>
+        <section className="mt-16">
+          <div className="flex justify-around mx-auto">
+            <DogCard/>
+            <DogCard/>
+            <DogCard/>
+          </div>
+            
+        </section>
+        </>
     )
 }
