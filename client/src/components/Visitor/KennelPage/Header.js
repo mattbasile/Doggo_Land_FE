d6062a9efa7c9a97e5eb114075492d31cf6b3211
Matt Basile <matthewbasile2@gmail.com>
2019-05-26 07:24:17 -0400

    updating kennel header

--- client/src/components/Visitor/KennelPage/Header.js
+++ client/src/components/Visitor/KennelPage/Header.js
@@ -4,13 +4,33 @@ import './kennel-page.css'
 
 export default function Header() {
     return (
-        <>
-            <div className="kennel-header">
+        <section className="flex">
+            <div className="image-header">
 
             </div>
-            <div className="">
+            <div className="text-header p-8">
+                <h2>Benhurst Kennel</h2>
+                <p>
+                The Benhurst Kennel has been around fron 25 years. They’ve connected over 10,000 families with their new best friends. The Benhurst Kennel has been around fron 25 years. They’ve connected over 10,000 families with their new best friends. The Benhurst Kennel has been around fron 25 years. They’ve connected over 10,000 families with their new best friends.
+                </p>
+            <div className="flex w-full items-baseline ">
+                <div className="flex ">
+                    <i className="fas fa-dog text-2xl"></i>
+                    <p className="text-xl ml-2">12</p>
+                </div>
+                <div className="flex">
+                    <i className="fas fa-home text-xl"></i>
+                    <p className="body-font font-normal ml-2 text-md"> Benhurst, New York</p>
+                </div>
+                <div className="flex items-baseline ">
+                    <i className="fas fa-phone text-xl"></i>
+                    <p className="body-font font-normal ml-2 ">(111)-222-3333</p>
+                </div>
 
+            </div>
             </div> 
-        </>
+
+
+        </section>
     )
 }
