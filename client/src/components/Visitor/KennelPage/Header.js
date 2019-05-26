e61d3c4612eb6c00e187ae5f9b7056486e5a32a3
Matt Basile <matthewbasile2@gmail.com>
2019-05-26 07:34:54 -0400

    finished kennel header

--- client/src/components/Visitor/KennelPage/Header.js
+++ client/src/components/Visitor/KennelPage/Header.js
@@ -8,12 +8,12 @@ export default function Header() {
             <div className="image-header">
 
             </div>
-            <div className="text-header p-8">
-                <h2>Benhurst Kennel</h2>
-                <p>
+            <div className="text-header p-12">
+                <h2 className="text-2xl font-bold body-font">Benhurst Kennel</h2>
+                <p className="text py-4 pr-4">
                 The Benhurst Kennel has been around fron 25 years. They’ve connected over 10,000 families with their new best friends. The Benhurst Kennel has been around fron 25 years. They’ve connected over 10,000 families with their new best friends. The Benhurst Kennel has been around fron 25 years. They’ve connected over 10,000 families with their new best friends.
                 </p>
-            <div className="flex w-full items-baseline ">
+            <div className="flex w-full items-baseline justify-between my-4 pr-4">
                 <div className="flex ">
                     <i className="fas fa-dog text-2xl"></i>
                     <p className="text-xl ml-2">12</p>
@@ -26,9 +26,12 @@ export default function Header() {
                     <i className="fas fa-phone text-xl"></i>
                     <p className="body-font font-normal ml-2 ">(111)-222-3333</p>
                 </div>
-
             </div>
+            <button className="contact-btn ">
+                Contact Kennel
+            </button>
             </div> 
+
 
 
         </section>
